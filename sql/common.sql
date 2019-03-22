e2a800930f96a8c123c2a642e3bed861177790b3
Paul Henshaw <paul@openquake.org>
2019-03-22 15:13:12 +0100

    Revised schema based on discussions with GFDRR SF

--- sql/common.sql
+++ sql/common.sql
@@ -3,6 +3,8 @@
 -- Hazard enumeration (copied from MOVER)
 -- License table
 --
+START TRANSACTION;
+
 CREATE SCHEMA IF NOT EXISTS cf_common;
 COMMENT ON SCHEMA cf_common IS 
 	'Common elements for all Challenge Fund Database Schemas';
@@ -25,6 +27,25 @@ CREATE TYPE cf_common.hazard_enum AS ENUM (
 );
 
 --
+-- Enumerated type for occupancy/use categories
+--
+CREATE TYPE cf_common.occupancy_enum AS ENUM (
+	'Residential',
+	'Commercial',
+	'Industrial',
+	'Infrastructure',
+	'Healthcare',
+	'Educational',
+	'Government',
+	'Crop',
+	'Livestock',
+	'Forestry',
+	'Mixed'
+);
+COMMENT ON TYPE cf_common.occupancy_enum
+	IS 'Types of Occupancy or building/structure use';
+
+--
 -- Supported licenses
 --
 CREATE TABLE IF NOT EXISTS cf_common.license (
@@ -39,5 +60,17 @@ COMMENT ON TABLE cf_common.license IS
 
 DELETE FROM cf_common.license;
 COPY cf_common.license (code,name,notes,url)
-	FROM '/tmp/licenses.csv'
-	WITH (FORMAT csv, header);
+	FROM STDIN
+	WITH (FORMAT csv);
+CC0,Creative Commons CCZero (CC0),Dedicate to the Public Domain (all rights waived),https://creativecommons.org/publicdomain/zero/1.0/
+PDDL,Open Data Commons Public Domain Dedication and Licence (PDDL),Dedicate to the Public Domain (all rights waived),https://opendatacommons.org/licenses/pddl/index.html
+CC-BY-4.0,Creative Commons Attribution 4.0 (CC-BY-4.0),,https://creativecommons.org/licenses/by/4.0/
+ODC-BY,Open Data Commons Attribution License(ODC-BY),Attribution for data(bases),https://opendatacommons.org/licenses/by/summary/
+CC-BY-SA-4.0,Creative Commons Attribution Share-Alike 4.0 (CC-BY-SA-4.0),,http://creativecommons.org/licenses/by-sa/4.0/
+ODbL,Open Data Commons Open Database License (ODbL),Attribution-ShareAlike for data(bases),https://opendatacommons.org/licenses/odbl/summary/
+\.
+
+COMMIT;
+
+-- Magic Vim comment to use 4 space tabs
+-- vim: set ts=4:sw=4
