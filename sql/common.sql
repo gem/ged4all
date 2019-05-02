fa0cdc34f18cd9c8a59d7194e1cc11032d90fee6
Paul Henshaw <paul@openquake.org>
2019-05-02 09:59:05 +0200

    Updated common in line with othe CF DBs

--- sql/common.sql
+++ sql/common.sql
@@ -1,6 +1,6 @@
 --
 -- Common elements for all Challenge Fund Database Schemas
--- Hazard enumeration (copied from MOVER)
+-- Hazard enumeration (Based on discussions with Stuart Fraser, GFDRR)
 -- License table
 --
 START TRANSACTION;
@@ -10,21 +10,77 @@ COMMENT ON SCHEMA cf_common IS
 	'Common elements for all Challenge Fund Database Schemas';
 
 --
--- Enumerated type for supported hazards (aka perils)
+-- Remove old Enumerated type for supported hazards (aka perils) 
 --
--- This will fail if the enum already exists.
+DROP TYPE IF EXISTS cf_common.hazard_enum;
+
+
 --
-CREATE TYPE cf_common.hazard_enum AS ENUM (
-    'Earthquake',
-    'Tsunami',
-    'Flood',
-    'Wind',
-    'Landslide',
-    'Storm Surge',
-    'Volcanic Ash',
-    'Drought',
-    'Multi-hazard'
+-- Valid hazard types
+--
+CREATE TABLE cf_common.hazard_type (
+    code    VARCHAR PRIMARY KEY,
+    name    VARCHAR NOT NULL
 );
+COMMENT ON TABLE cf_common.hazard_type IS 'Valid Hazard types';
+ALTER TABLE cf_common.hazard_type OWNER TO losscontrib;
+
+DELETE FROM cf_common.hazard_type;
+COPY cf_common.hazard_type (code, name) FROM stdin;
+CS	Convective Storm
+EQ	Earthquake
+TS	Tsunami
+VO	Volcanic
+CF	Coastal Flood
+FL	Flood
+LS	Landslide
+WI	Strong Wind
+ET	Extreme Temperature
+DR	Drought
+WF	Wildfire
+MH	Multi-Hazard
+\.
+
+--
+-- Process Types by Hazard type
+--
+CREATE TABLE cf_common.process_type (
+    code        VARCHAR PRIMARY KEY,
+    hazard_code VARCHAR NOT NULL,
+    name        VARCHAR NOT NULL
+);
+ALTER TABLE cf_common.process_type OWNER TO losscontrib;
+
+DELETE FROM cf_common.process_type;
+COPY cf_common.process_type (code, hazard_code, name) FROM stdin;
+QLI	EQ	Liquefaction
+QGM	EQ	Ground Motion
+Q1R	EQ	Primary Rupture
+Q2R	EQ	Secondary Rupture
+TSI	TS	Tsunami
+VAF	VO	Ashfall
+VLH	VO	Lahar
+VPF	VO	Pyroclastic Flow
+VBL	VO	Ballistics
+VLV	VO	Lava
+VFH	VO	Proximal hazards
+FSS	CF	Storm Surge
+FCF	CF	Coastal Flood
+FFF	FL	Fluvial Flood
+FPF	FL	Pluvial Flood
+LAV	LS	Snow Avalanche
+LSL	LS	Landslide (general)
+TCY	WI	Tropical cyclone
+ETC	WI	Extratropical cyclone
+EHT	ET	Extreme heat
+ECD	ET	Extreme cold
+DTS	DR	Socio-economic Drought
+DTM	DR	Meteorological Drought
+DTH	DR	Hydrological Drought
+DTA	DR	Agricultural Drought
+WFI	WF	Wildfire
+TOR	CS	Tornado
+\.
 
 --
 -- Enumerated type for occupancy/use categories
@@ -42,7 +98,7 @@ CREATE TYPE cf_common.occupancy_enum AS ENUM (
 	'Forestry',
 	'Mixed'
 );
-COMMENT ON TYPE cf_common.occupancy_enum
+COMMENT ON TYPE cf_common.occupancy_enum 
 	IS 'Types of Occupancy or building/structure use';
 
 --
@@ -57,6 +113,7 @@ CREATE TABLE IF NOT EXISTS cf_common.license (
 );
 COMMENT ON TABLE cf_common.license IS
 	'List of supported licenses';
+ALTER TABLE cf_common.process_type OWNER TO losscontrib;
 
 DELETE FROM cf_common.license;
 COPY cf_common.license (code,name,notes,url)
@@ -71,6 +128,10 @@ ODbL,Open Data Commons Open Database License (ODbL),Attribution-ShareAlike for d
 \.
 
 COMMIT;
+
+GRANT USAGE ON SCHEMA cf_common TO lossusers;
+GRANT SELECT ON ALL TABLES IN SCHEMA cf_common TO lossusers;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA cf_common TO losscontrib;
 
 -- Magic Vim comment to use 4 space tabs
 -- vim: set ts=4:sw=4
