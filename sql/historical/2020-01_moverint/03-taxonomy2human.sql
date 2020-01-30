c721d82bf7e581c309d81615c59e478ac9ce6213
Paul Henshaw <paul@openquake.org>
2020-01-31 00:47:46 +0100

    GED4ALL taxonomy and model cleanup, added taxonomy explain function, moved older updates into dedicated folder

---
+++ sql/historical/2020-01_moverint/03-taxonomy2human.sql
@@ -0,0 +1,18 @@
+-- HOWTO use it:
+--  . install oq-platform-taxtweb python package system-wide
+--  . use CREATE LANGUAGE plpython3u; to be able to run it
+--  . load this function once in the database
+CREATE OR REPLACE FUNCTION cf_common.taxonomy2human (taxonomy text) RETURNS text AS 
+    $$
+        from openquakeplatform_taxtweb.utils.taxonomy2human import taxonomy2human as taxonomy2human_py
+
+        return taxonomy2human_py(taxonomy)
+    $$ 
+	LANGUAGE plpython3u 
+	IMMUTABLE 
+	SECURITY INVOKER 
+	RETURNS NULL ON NULL INPUT
+	PARALLEL SAFE;
+
+COMMENT ON FUNCTION cf_common.taxonomy2human IS 
+	'Returns an English language description of the given string in the GEM Taxonomy v2.0 format.';
